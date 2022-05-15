a14ec140a7be68d4fdd36e1ec6b0028f98a2f3bc
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-15 17:41:58 -0300

    Create custom shadow-card Tailwind extension

--- laravello/tailwind.config.js
+++ laravello/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue",],
     theme:   {
-        extend: {},
+        extend: {
+            boxShadow: {
+                card: '0 1px 0 rgba(9, 30, 66, 0.25)'
+            }
+        },
     },
     plugins: [],
 };
