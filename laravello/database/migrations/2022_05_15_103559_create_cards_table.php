1f70e1f1ac189164e0a74fbe0c8445a640cea792
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-15 08:47:54 -0300

    Add models and migrations

---
+++ laravello/database/migrations/2022_05_15_103559_create_cards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\CardList;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->smallInteger('order');
+            $table->foreignIdFor(CardList::class, 'list_id')->constrained(
+                'card_lists'
+            );
+            $table->foreignIdFor(User::class, 'owner_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
