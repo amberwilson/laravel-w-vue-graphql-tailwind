ed38dbb97b302edbf48382783d8a7c17d301b423
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-17 19:31:39 -0300

    Setup card edit and delete placeholders

--- laravello/resources/js/components/CardItem.vue
+++ laravello/resources/js/components/CardItem.vue
@@ -1,8 +1,18 @@
 <template>
   <div
-    class="shadow-card bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2"
+    class="flex justify-between group shadow-card bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 "
   >
-    {{ card.title }}
+    <div>{{ card.title }}</div>
+    <div
+      class="flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-500"
+    >
+      <div class="text-gray-400 pr-2 hover:text-gray-500">
+        E
+      </div>
+      <div class="text-gray-400 hover:text-gray-500">
+        D
+      </div>
+    </div>
   </div>
 </template>
 
