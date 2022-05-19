a7e764039ce13d19689e53c3f06ddf7e866404e3
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-19 06:25:01 -0300

    Setup login page

--- laravello/resources/js/pages/LoginPage.vue
+++ laravello/resources/js/pages/LoginPage.vue
@@ -1,5 +1,51 @@
 <template>
-  Login Page
+  <div class="bg-white sm:bg-gray-100 h-full flex justify-center">
+    <div class="container mt-2 sm:mt-10 flex flex-col items-center">
+      <div class="text-3xl text-blue-700 font-bold mb-10">
+        <span>Laravello</span>
+      </div>
+      <div class="w-full sm:shadow-xl sm:bg-white sm:py-8 sm:px-12">
+        <div class="w-full text-center text-gray-600 font-bold mb-8">
+          Log in to
+          Laravello
+        </div>
+        <form action="">
+          <div class="w-full mb-4">
+            <input
+              type="text"
+              placeholder="Enter email"
+              class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
+            >
+          </div>
+          <div class="w-full mb-4">
+            <input
+              type="password"
+              placeholder="Enter password"
+              class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
+            >
+          </div>
+          <div class="w-full mb-6">
+            <button
+              type="submit"
+              class="rounded-sm px-4 py-2 text-sm bg-green-500 font-bold outline-none focus:outline-none hover:bg-opacity-75 w-full active:bg-green-600 text-white disabled:opacity-25"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <div class="bg-gray-400 h-px w-full mb-6" />
+
+        <div class="text-center text-sm">
+          <a
+            href="#"
+            class="text-blue-600 hover:underline"
+          >Sign up for an
+            account</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +53,15 @@
 </script>
 
 <style scoped>
+.container {
+  width: 300px;
+  max-width: 300px;
+}
 
+@media (min-width: 640px) {
+  .container {
+    width: 400px;
+    max-width: 400px;
+  }
+}
 </style>
