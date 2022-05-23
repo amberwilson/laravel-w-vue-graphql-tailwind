52f2e515967c4a7a8b7232ec29c7865079fc3d21
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-23 07:54:03 -0300

    Setup Logout, Login, and Register buttons on board page

--- laravello/resources/js/pages/BoardPage.vue
+++ laravello/resources/js/pages/BoardPage.vue
@@ -6,7 +6,7 @@
       <div
         class="header ml-2 w-1/3"
       >
-        X
+        x
       </div>
       <div class="text-lg opacity-50 cursor-pointer hover:opacity-75">
         Laravello
@@ -14,7 +14,28 @@
       <div
         class="mr-2 w-1/3 flex justify-end"
       >
-        {{ `Logged in as ${user.name}` }}
+        <div v-if="user.id">
+          <button class="header-btn">
+            Logout
+          </button>
+        </div>
+        <div
+          v-else
+          class="flex justify-end"
+        >
+          <button
+            class="header-btn"
+            @click="router.push({name: 'login'})"
+          >
+            Login
+          </button>
+          <button
+            class="header-btn"
+            @click="router.push({name: 'register'})"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
 
@@ -46,9 +67,12 @@ import { useUserStore } from '@stores/user'
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 import BOARD_QUERY from '../gql/BoardWithListsAndCards.gql'
 
 const user = useUserStore()
+
+const router = useRouter()
 
 const {
   result, // error,
