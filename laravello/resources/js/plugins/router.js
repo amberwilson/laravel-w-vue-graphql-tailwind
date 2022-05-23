b6fa70befdd400f8bfa333c9c80eb2e43b9c8cbd
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-23 16:29:49 -0300

    Check that user is logged in when navigating

--- laravello/resources/js/plugins/router.js
+++ laravello/resources/js/plugins/router.js
@@ -1,6 +1,7 @@
 import BoardPage from '@pages/BoardPage'
 import LoginPage from '@pages/LoginPage'
 import RegisterPage from '@pages/RegisterPage'
+import { useUserStore } from '@stores/user'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -16,6 +17,38 @@ const routes = [
         component: RegisterPage,
     }]
 
-export const router = createRouter({
+const router = createRouter({
     history: createWebHistory(), routes,
 })
+
+router.beforeEach(async (to) => {
+    const user = useUserStore()
+
+    if (!user.id) {
+        if (to.name !== 'login') {
+            return { name: 'login' }
+        }
+    } else {
+        try {
+            const me = await user.fetchCurrentUser()
+
+            if (!me) {
+                // session expired
+                return { name: 'login' }
+            }
+
+            if (to.name !== 'board') {
+                return { name: 'board' }
+            }
+        } catch (exception) {
+            console.log('Exception while trying to fetch the user', exception)
+            user.clearUser()
+
+            if (to.name !== 'login') {
+                return { name: 'login' }
+            }
+        }
+    }
+})
+
+export { router }
