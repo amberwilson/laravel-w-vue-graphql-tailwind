4a5a4f5b7c398097ec19c881552967e5cfcd48c9
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-23 16:29:00 -0300

    Add clearUser and fetchCurrentUser to user store

--- laravello/resources/js/stores/user.js
+++ laravello/resources/js/stores/user.js
@@ -1,11 +1,18 @@
+import { provideApolloClient } from '@vue/apollo-composable'
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import ME from '../gql/Me.gql'
+import { apolloClient } from '../plugins/apollo.js'
 
+const defaultState = {
+    id: null, email: null, name: null,
+}
+
+/**
+ *
+ * @type {StoreDefinition<"user", {name: null, id: null, email: null}, {}, {clearUser(): void, setUser(Integer, String, String): void, fetchCurrentUser(): Promise<*>}>}
+ */
 export const useUserStore = defineStore('user', {
-    state: () => ({
-        id: null,
-        email: null,
-        name: null,
-    }),
+    state: () => defaultState,
 
     actions: {
         /**
@@ -17,6 +24,40 @@ export const useUserStore = defineStore('user', {
         setUser (id, email, name) {
             this.$patch({
                 id: Number(id), email, name,
+            })
+        },
+
+        /**
+         * Clear the current user
+         */
+        clearUser () {
+            this.$patch(defaultState)
+        },
+
+        /**
+         * Fetch the user for the current session
+         * @returns {Promise<unknown>}
+         */
+        async fetchCurrentUser () {
+            provideApolloClient(apolloClient)
+
+            return new Promise((resolve, reject) => {
+                try {
+                    apolloClient.query({
+                        query: ME, fetchPolicy: 'no-cache',
+                    }).then((result) => {
+                        if (result.data?.me) {
+                            const { id, email, name } = result.data.me
+                            this.setUser(id, email, name)
+
+                            resolve(result.data.me)
+                        } else {
+                            resolve(null)
+                        }
+                    })
+                } catch (exception) {
+                    reject('Exception while retrieving `me`', exception)
+                }
             })
         },
     },
