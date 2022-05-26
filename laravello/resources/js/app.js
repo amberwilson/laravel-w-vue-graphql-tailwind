3067692b7e90839b3276f606860bd6c4e5089964
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-26 07:33:04 -0300

    Install vue3-click-away

--- laravello/resources/js/app.js
+++ laravello/resources/js/app.js
@@ -1,6 +1,7 @@
 import App from '@components/App'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { createApp, h, provide } from 'vue'
+import VueClickAway from 'vue3-click-away'
 import { apolloClient } from './plugins/apollo'
 import { pinia } from './plugins/pinia'
 import { router } from './plugins/router'
@@ -13,4 +14,5 @@ createApp({
 })
     .use(router)
     .use(pinia)
+    .use(VueClickAway)
     .mount('#app')
