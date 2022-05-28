24acf44db671580799c52b4a4eedd9029ea0231e
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-28 11:11:29 -0300

    Add owner to board model when it's being created

--- laravello/app/Models/Board.php
+++ laravello/app/Models/Board.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * @mixin IdeHelperBoard
@@ -24,5 +25,16 @@ class Board extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    protected static function boot()
+    {
+        static::creating(static function (Board $board) {
+            if (Auth::user()) {
+                $board->owner()->associate(Auth::user());
+            }
+        });
+
+        parent::boot();
     }
 }
