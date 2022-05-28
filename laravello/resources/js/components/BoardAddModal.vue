823f1b320ee632e6627ef39b24fbea6124b68a87
amberwilson <678866+amberwilson@users.noreply.github.com>
2022-05-28 11:12:27 -0300

    Wire-up creating the board and updating query cache

--- laravello/resources/js/components/BoardAddModal.vue
+++ laravello/resources/js/components/BoardAddModal.vue
@@ -10,6 +10,7 @@
       >
         <input
           v-model="title"
+          v-focus
           type="text"
           placeholder="Add board title"
           class="title rounded-sm text-white outline-none p-2 font-bold w-full hover:opacity-50 placeholder-gray-100"
@@ -33,8 +34,10 @@
     </div>
     <div class="mt-4">
       <button
-        class="rounded-sm py-2 px-4 text-white hover:opacity-75 cursor-pointer"
+        class="rounded-sm py-2 px-4 text-white hover:opacity-75 cursor-pointer disabled:opacity-25 disabled:cursor-not-allowed"
         :class="[colorMap500[color]]"
+        :disabled="cannotCreate"
+        @click="createBoard"
       >
         Create
       </button>
@@ -45,13 +48,21 @@
 <script setup>
 import BoardColor from '@components/BoardColor'
 import ModalBox from '@components/ModalBox'
-import { defineEmits, defineProps, ref } from 'vue'
+import { useMutation } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { computed, defineEmits, defineProps, ref } from 'vue'
+import BOARD_ADD from '../gql/BoardAdd.gql'
 import { colorGrid, colorMap500 } from '../utils'
 
 defineProps({
   show: { type: Boolean, default: true },
 })
 const emit = defineEmits(['closed'])
+
+const vFocus = {
+  mounted: (el) => el.focus(),
+}
+
 const color = ref('orange')
 const title = ref(null)
 
@@ -59,6 +70,48 @@ function close () {
   emit('closed')
 }
 
+const cannotCreate = computed(() => title.value === null || title.value === '')
+
+const {
+  mutate: boardAddMutation,
+} = useMutation(BOARD_ADD, {
+  update: (cache, { data: { boardAdd } }) => {
+    cache.modify({
+      id: 'ROOT_QUERY',
+      fields: {
+        userBoards (existingBoardRefs = [], { readField }) {
+          const newBoardRef = cache.writeFragment({
+            data: boardAdd,
+            fragment: gql`
+              fragment NewBoard on Board {
+                id
+                title
+                color
+              }
+            `,
+          })
+
+          if (existingBoardRefs.some(
+            ref => readField('id', ref) === boardAdd.id,
+          )) {
+            return existingBoardRefs
+          }
+
+          return [...existingBoardRefs, newBoardRef]
+        },
+      },
+    })
+
+    emit('closed')
+  },
+})
+
+function createBoard () {
+  boardAddMutation({
+    title: title.value,
+    color: color.value,
+  })
+}
 </script>
 
 <style scoped>
